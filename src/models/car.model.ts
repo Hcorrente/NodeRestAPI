be51c1c8e20807a71840fa42fa640963390ac9ab
Hcorrente <hcorrente@mail.fresnostate.edu>
2019-05-22 15:38:17 -0700

    car model changes

--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -8,6 +8,7 @@ export class Car {
             make: { type: String, maxlength: 24 },
             model: { type: String, maxlength: 24 },
             year: { type: String, maxlength: 24 },
+            color: { type: String, maxlength: 24 },
             mileage: { type: String, maxlength: 24 },
             user_id: {
                 type: Number,
@@ -23,21 +24,63 @@ export class Car {
                 method: 'POST',
                 callback: this.getAllCars,
                 requireToken: true,
-            }, 
+            },
             {
                 route: '/get-car-by-id/:id',
                 method: 'POST',
                 callback: this.getCarById,
                 requireToken: true,
+            }, {
+                route: '/create-car',
+                method: 'POST',
+                callback: this.createCar,
+                requireToken: true,
+            }, {
+                route: '/update-car/id/:id',
+                method: 'PUT',
+                callback: this.updateCar,
+                requireToken: true,
+            }, {
+                route: '/delete/id/:id',
+                method: 'DELETE',
+                callback: this.deleteCar,
+                requireToken: true,
             }
         ]];
     }
 
+    updateCar(model: any) {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            console.log('req.body===>', req.body);
+            let carCtrl = model.controller;
+            let resp = await carCtrl.update(req, null, null);
+            res.json({ message: 'Success', resp });
+        }
+    }
+
+    deleteCar(model: any) {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            console.log('req.body===>', req.body);
+            let carCtrl = model.controller;
+            let resp = await carCtrl.remove(req, null, null);
+            res.json({ message: 'Success', resp });
+        }
+    }
+
+    createCar(model: any) {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            console.log('req.body===>', req.body);
+            let carCtrl = model.controller;
+            let resp = await carCtrl.insert(req, null, null);
+            res.json({ message: 'Success', resp });
+        }
+    }
+
     getAllCars(model: any) {
         return async (req: Request, res: Response, next: NextFunction) => {
-                req.body = {
-                    get: ["*"]
-                }
+            req.body = {
+                get: ["*"]
+            }
             let carCtrl = model.controller;
             let resp = await carCtrl.get(req, null, null);
             res.json({ message: 'Success', resp });
@@ -46,24 +89,24 @@ export class Car {
 
     getCarById(model: any) {
         return async (req: Request, res: Response, next: NextFunction) => {
-                req.body = {
-                    get: ["*"],
-                    where: {
-                        id: req.params.id
-                    }
+            req.body = {
+                get: ["*"],
+                where: {
+                    id: req.params.id
                 }
+            }
             let carCtrl = model.controller;
             let resp = await carCtrl.get(req, null, null);
             res.json({ message: 'Success', resp });
         }
     }
 
-            set model(model: any) {
-                this._model = model;
-            }
+    set model(model: any) {
+        this._model = model;
+    }
 
-            get model() {
-                return this._model;
-            }
+    get model() {
+        return this._model;
+    }
 
-        }
+}
