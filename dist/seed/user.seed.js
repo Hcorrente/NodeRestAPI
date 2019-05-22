ce01600d17ee6e821a8aa285f768c2269b20379b
Hcorrente <hcorrente@mail.fresnostate.edu>
2019-05-22 13:26:31 -0700

    car model

--- dist/seed/user.seed.js
+++ dist/seed/user.seed.js
@@ -1,8 +1,8 @@
 [{
   "id": 1,
-  "email": "lphan@shift3tech.com",
-  "username": "lphan",
-  "password": "abc123",
+  "email": "hcorrente@mail.fresnostate.edu",
+  "username": "hcorrente",
+  "password": "Sadieismydog1125!",
   "super_admin": true,
   "site_admin": true,
   "hash": "CVI2C3WKYEdbL4yi3B9eE",
